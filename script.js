8c160abe87c716680c1f6de66a1226960fb7107c
Roman <1progroma@gmail.com>
2020-11-14 13:23:59 +0200

    Assignment 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Sample of object and usage of prompt*/ 
-const numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
 console.log(numberOfFilms);
 console.log(`Я посмотрел ${numberOfFilms} фильмов`);
 const personalMovieDB = {
@@ -9,8 +9,25 @@ const personalMovieDB = {
     genres:[],
     privat: false
 };
-    for (let i = 0; i < 2; i++) {
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {alert('Произошла ошибка');}
+
+
+tryAgain:
+for (let i = 0; i < 3; i++) {
         let propTemp = prompt ('Один из последних просмотренных фильмов?','');
-        personalMovieDB.movies[propTemp] = prompt ('На сколько оцените его?',''); 
-    }
+        if (propTemp === '' || propTemp === null || propTemp.length > 50) {
+            i = 0;
+            alert('Введите значение менше 50 символов');
+            continue tryAgain;
+        }
+        personalMovieDB.movies[propTemp] = prompt ('На сколько оцените его?','');
+}
+
 console.log(personalMovieDB);
